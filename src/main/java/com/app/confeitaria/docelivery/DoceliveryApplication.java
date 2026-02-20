b2edc5c6c0faacda00375924371058b089b9111e
Marina980 <marina.biaeadao@gmail.com>
2026-02-20 20:07:54 -0300

    Projeto confeitaria aula professor Rogerio

---
+++ src/main/java/com/app/confeitaria/docelivery/DoceliveryApplication.java
@@ -0,0 +1,13 @@
+package com.app.confeitaria.docelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoceliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoceliveryApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/app/confeitaria/docelivery/model/entity/Usuario.java
@@ -0,0 +1,28 @@
+package com.app.confeitaria.docelivery.model.entity;
+
+import java.time.LocalDate;
+
+public class Usuario {
+    private int id;
+    private String nome;
+    private String apelido;
+    private String cpf;
+    private String cep;
+    private String endereco;
+    private String bairro;
+    private String cidade;
+    private String uf;
+    private String telefone;
+    private String email;
+    private String senha;
+    private String cnh;
+    private String placaVeiculo;
+    private String veiculo;
+    private String nomeLoja;
+    private String proprietario;
+    private String categoria;
+    private String promocao;
+    private LocalDate dataNascimento;
+
+
+}
